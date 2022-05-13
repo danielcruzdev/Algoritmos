e0a6a4b5693126ed45af76313957bf70af54751c
Daniel Cruz <d.afc98@gmail.com>
2022-05-12 23:26:06 -0300

    algoritmo 41

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo41.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo41
+    {
+        /*
+            Faça um algoritmo que mostre na tela a seguinte contagem:
+            100 95 90 85 80 ... 0 Acabou!
+        */
+        public static void Start()
+        {
+            var i = 100;
+            var fim = 0;
+            while(i >= fim)
+            {
+                Console.Write($"{i} ");
+                i-=5;
+            }
+            Console.WriteLine("Acabou!");
+        }
+    }
+}
