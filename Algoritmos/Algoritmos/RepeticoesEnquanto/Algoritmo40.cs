8d4978e3f7da5655a22d79beafa5f8af6e54ec66
Daniel Cruz <d.afc98@gmail.com>
2022-05-12 23:24:38 -0300

    algoritmo 40

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo40.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo40
+    {
+        /*
+            Faça um algoritmo que mostre na tela a seguinte contagem:
+            0 3 6 9 12 15 18 Acabou!
+        */
+        public static void Start()
+        {
+            var i = 0;
+            var fim = 18;
+            while(i <= fim)
+            {
+                Console.Write($"{i} ");
+                i+=3;
+            }
+            Console.WriteLine("Acabou!");
+        }
+    }
+}
