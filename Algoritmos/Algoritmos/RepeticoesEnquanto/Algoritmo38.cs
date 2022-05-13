3f2b00707369a692270dc5f5313e4fe9e264f2cf
Daniel Cruz <d.afc98@gmail.com>
2022-05-12 23:20:56 -0300

    algoritmo 38

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo38.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo38
+    {
+        /*
+            Escreva um programa que mostre na tela a seguinte contagem:
+            6 7 8 9 10 11 Acabou!
+        */
+        public static void Start()
+        {
+            var i = 6;
+            var fim = 11;
+            while(i <= fim)
+            {
+                Console.Write($"{i} ");
+                i++;
+            }
+            Console.WriteLine("Acabou!");
+        }
+    }
+}
