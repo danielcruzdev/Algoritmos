be6ced1b585dc08c9d8fb3aed1a6b75641b11782
Daniel Cruz <d.afc98@gmail.com>
2022-05-12 23:21:06 -0300

    algoritmo 39

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo39.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo39
+    {
+        /*
+            Faça um algoritmo que mostre na tela a seguinte contagem:
+            10 9 8 7 6 5 4 3 Acabou!
+        */
+        public static void Start()
+        {
+            var i = 10;
+            var fim = 3;
+            while(i >= fim)
+            {
+                Console.Write($"{i} ");
+                i--;
+            }
+            Console.WriteLine("Acabou!");
+        }
+    }
+}
