c19e52fb23885982cf7278bfd49fe924c96be0f8
Daniel Cruz <d.afc98@gmail.com>
2022-05-15 18:25:28 -0300

    algoritmo 47

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo47.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo47
+    {
+        /*
+            Desenvolva um aplicativo que mostre na tela o resultado da expressão 500 +
+            450 + 400 + 350 + 300 + ... + 50 + 0
+        */
+        public static void Start()
+        {
+            var i = 500;
+            var last = 0;
+            var decrement = 50;
+            var total = 0;
+
+            while (i >= last)
+            {
+                total += i;
+                Console.Write($"{i} ");
+                i -= decrement;
+            }
+
+            Console.WriteLine("Acabou!");
+            Console.WriteLine($"Soma: {total}");
+        }
+    }
+}
