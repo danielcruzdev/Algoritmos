55eebf54be09bc276ce0c4a664e76d409391a5cc
Daniel Cruz <d.afc98@gmail.com>
2022-05-15 18:25:15 -0300

    algoritmo 46

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo46.cs
@@ -0,0 +1,28 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo46
+    {
+        /*
+            Crie um programa que calcule e mostre na tela o resultado da soma entre 6 +
+            8 + 10 + 12 + 14 + ... + 98 + 100.
+        */
+        public static void Start()
+        {
+            var i = 6;
+            var last = 100;
+            var increment = 2;
+            var total = 0;
+
+            while (i <= last)
+            {
+                total += i;
+                Console.Write($"{i} ");
+                i += increment;
+            }
+
+
+            Console.WriteLine("Acabou!");
+            Console.WriteLine($"Soma: {total}");
+        }
+    }
+}
