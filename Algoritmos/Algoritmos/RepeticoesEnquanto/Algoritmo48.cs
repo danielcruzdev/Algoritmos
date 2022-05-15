3edd6da90c1a57a7fa7233ed64222fd919e15df0
Daniel Cruz <d.afc98@gmail.com>
2022-05-15 18:34:17 -0300

    algoritmo 48

---
+++ Algoritmos/Algoritmos/RepeticoesEnquanto/Algoritmo48.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo48
+    {
+        /*
+            Faça um programa que leia 7 números inteiros e no final mostre o somatório
+            entre eles.
+        */
+        public static void Start()
+        {
+            var qtdNumeros = 7;
+            var i = 1;
+            var total = 0;
+
+            while (i <= qtdNumeros)
+            {
+                Console.Write($"Digite um número: ");
+                var numero = int.Parse(Console.ReadLine() ?? "0");
+
+                total += numero;
+                i++; ;
+            }
+
+            Console.WriteLine($"Soma: {total}");
+        }
+    }
+}
