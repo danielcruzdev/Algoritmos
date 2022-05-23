58b767cbc628c1ead59a67d1c7f9c919e4c300c0
Daniel Cruz <d.afc98@gmail.com>
2022-05-22 23:14:55 -0300

    algoritmo 65

---
+++ Algoritmos/Algoritmos/Passo7/Algoritmo65.cs
@@ -0,0 +1,20 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo65
+    {
+        /*
+            Desenvolva um programa usando a estrutura “para” que mostre na tela a
+            seguinte contagem:
+            0 5 10 15 20 25 30 35 40 Acabou!
+        */
+        public static void Start()
+        {
+            for (int i = 100; i >= 0; i-=10)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.Write("Acabou!");
+        }
+    }
+}
