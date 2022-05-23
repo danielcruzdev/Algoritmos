dff7ca625e0f1a118aa44db16aa5484c92fe6018
Daniel Cruz <d.afc98@gmail.com>
2022-05-22 22:58:29 -0300

    algoritmo 61

---
+++ Algoritmos/Algoritmos/Passo6/Algoritmo61.cs
@@ -0,0 +1,25 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo61
+    {
+        /*
+            Crie um programa que mostre na tela a seguinte contagem, usando a estrutura
+            “faça enquanto”
+            0 3 6 9 12 15 18 21 24 27 30 Acabou!
+        */
+        public static void Start()
+        {
+            var i = 0;
+            var fim = 30;
+            var incremento = 3;
+            do
+            {
+                Console.Write($"{i} ");
+                i += incremento;
+
+            } while (i <= fim);
+
+            Console.Write("Acabou!");
+        }
+    }
+}
