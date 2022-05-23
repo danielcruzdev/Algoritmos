5e1f1c0af96bda5382c96e115e0e83dddb39fff0
Daniel Cruz <d.afc98@gmail.com>
2022-05-22 23:14:45 -0300

    algoritmo 64

---
+++ Algoritmos/Algoritmos/Passo7/Algoritmo64.cs
@@ -0,0 +1,20 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo64
+    {
+        /*
+            Desenvolva um programa usando a estrutura “para” que mostre na tela a
+            seguinte contagem:
+            0 5 10 15 20 25 30 35 40 Acabou!
+        */
+        public static void Start()
+        {
+            for (int i = 0; i <= 40; i+=5)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.Write("Acabou!");
+        }
+    }
+}
