3db302b0798cc1eb9a08d1acc7b4dab37ea65235
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:03:49 -0300

    Algoritmo 74

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo74.cs
@@ -0,0 +1,29 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo74
+    {
+        /*
+            Crie um programa que preencha automaticamente (usando lógica, não apenas
+            atribuindo diretamente) um vetor numérico com 10 posições, conforme abaixo:
+                                5 3 5 3 5 3 5 3 5 3
+                                0 1 2 3 4 5 6 7 8 9       
+        */
+
+        public static void Start()
+        {
+            int[] numeros = new int[10];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int valor;
+                if (i % 2 == 0)
+                    valor = 5;
+                else
+                    valor = 3;
+
+                numeros[i] = valor;
+                Console.Write($"{numeros[i]} | ");
+            }
+        }
+    }
+}
