3bf7c1c6f698e9d69207d7ff187b1c23b69a2d71
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:04:04 -0300

    Algoritmo 76

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo76.cs
@@ -0,0 +1,24 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo76
+    {
+        /*
+            Crie um programa que preencha automaticamente um vetor numérico com 7
+            números gerados aleatoriamente pelo computador e depois mostre os valores
+            gerados na tela.     
+        */
+
+        public static void Start()
+        {
+            int[] numeros = new int[7];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                var random = new Random();
+                numeros[i] = random.Next(0, 1001);
+
+                Console.Write($"{numeros[i]} | ");
+            }
+        }
+    }
+}
