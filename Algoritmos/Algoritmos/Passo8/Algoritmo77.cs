b10f0ea1ecba98057909280864bdef146e7bcb1b
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:04:10 -0300

    Algoritmo 77

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo77.cs
@@ -0,0 +1,31 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo77
+    {
+        /*
+            Faça um programa que leia 7 nomes de pessoas e guarde-os em um vetor. No
+            final, mostre uma listagem com todos os nomes informados, na ordem inversa
+            daquela em que eles foram informados.    
+        */
+
+        public static void Start()
+        {
+            string[] nomes = new string[7];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write("Digite o nome: ");
+                var nome = Console.ReadLine();
+
+                nomes[i] = nome;
+            }
+
+            var index = 6;
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[index]);
+                index--;
+            }
+        }
+    }
+}
