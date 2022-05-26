23f237d7ff12275577f63072a6af6ae6ee8f180b
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:03:44 -0300

    Algoritmo 73

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo73.cs
@@ -0,0 +1,22 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo73
+    {
+        /*
+            Crie um programa que preencha automaticamente (usando lógica, não apenas
+            atribuindo diretamente) um vetor numérico com 10 posições, conforme abaixo:
+        */
+        public static void Start()
+        {
+            int[] numeros = new int[10];
+
+            var valor = 9;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = valor;
+                Console.Write($"{numeros[i]} | ");
+                valor--;
+            }
+        }
+    }
+}
