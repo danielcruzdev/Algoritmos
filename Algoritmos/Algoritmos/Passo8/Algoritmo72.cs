d865cbbe476a1a9b532ce7fae7d51f4a636dfcfc
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:03:39 -0300

    Algoritmo 72

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo72.cs
@@ -0,0 +1,26 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo72
+    {
+        /*
+            Crie um programa que preencha automaticamente (usando lógica, não apenas
+            atribuindo diretamente) um vetor numérico com 10 posições, conforme abaixo:
+        */
+        public static void Start()
+        {
+            int[] numeros = new int[10];
+
+            var valor = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (i != 0)
+                    valor += 5;
+                else
+                    valor = 5;
+
+                numeros[i] = valor;
+                Console.Write($"{numeros[i]} | ");
+            }
+        }
+    }
+}
