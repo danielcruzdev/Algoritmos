bce9e9fb64bcce58e32768c6e053e599621a2fdf
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:03:55 -0300

    Algoritmo 75

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo75.cs
@@ -0,0 +1,29 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo75
+    {
+        /*
+            Crie um programa que preencha automaticamente (usando lógica, não apenas
+            atribuindo diretamente) um vetor numérico com 15 posições com os primeiros
+            elementos da sequência de Fibonacci:       
+        */
+
+        public static void Start()
+        {
+            int[] numeros = new int[16];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (i == 0 || i == 1)
+                    numeros[i] = 1;
+                else
+                {
+                    var valor = numeros[i - 2] + numeros[i - 1];
+                    numeros[i] = valor;
+                }
+
+                Console.Write($"{numeros[i]} | ");
+            }
+        }
+    }
+}
