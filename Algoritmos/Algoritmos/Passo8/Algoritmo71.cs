65953f86443fc012d0bd55f332775fd57b6a9ffc
Daniel Cruz <d.afc98@gmail.com>
2022-05-25 23:03:30 -0300

    Algoritmo 71

---
+++ Algoritmos/Algoritmos/Passo8/Algoritmo71.cs
@@ -0,0 +1,20 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo71
+    {
+        /*
+            Faça um programa que preencha automaticamente um vetor numérico com 8
+            posições, conforme abaixo:
+        */
+        public static void Start()
+        {
+            int[] numeros = new int[8];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 999;
+                Console.Write($"{numeros[i]} | ");
+            }
+        }
+    }
+}
