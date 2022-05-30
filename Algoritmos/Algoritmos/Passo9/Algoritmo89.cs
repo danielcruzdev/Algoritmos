b719f1d77c72e7af0aaa0d5e6b93cf3a51c9b166
Daniel Cruz <d.afc98@gmail.com>
2022-05-29 23:21:13 -0300

    algoritmo 89

---
+++ Algoritmos/Algoritmos/Passo9/Algoritmo89.cs
@@ -0,0 +1,42 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo89
+    {
+        /*
+            Crie um programa que melhore o procedimento Gerador() da questão anterior
+            para que o programador possa escolher uma entre três bordas:
+             +-------=======------+ Borda 1
+             ~~~~~~~~:::::::~~~~~~~ Borda 2
+             <<<<<<<<------->>>>>>> Borda 3
+            Ex: Uma chamada válida seria Gerador("Portugol Studio", 3, 2)
+            ~~~~~~~~:::::::~~~~~~~
+             Portugol Studio
+             Portugol Studio
+             Portugol Studio
+            ~~~~~~~~:::::::~~~~~~~
+        */
+
+        public static void Start()
+        {
+            Gerador("Aprendendo C#", 4, 2);
+        }
+
+        private static void Gerador(string mensagem, int qtd, int borda)
+        {
+            string? bordaStyle = borda switch
+            {
+                1 => "+-------=======------+",
+                2 => "~~~~~~~~:::::::~~~~~~~",
+                3 => "<<<<<<<<------->>>>>>>",
+                _ => "----------------------",
+            };
+
+            Console.WriteLine(bordaStyle);
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+            Console.WriteLine(bordaStyle);
+        }
+    }
+}
