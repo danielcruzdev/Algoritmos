19e8f305eca2a2eabd1a9b0da58013fd9ef68241
Daniel Cruz <d.afc98@gmail.com>
2022-05-29 23:21:02 -0300

    algoritmo 87

---
+++ Algoritmos/Algoritmos/Passo9/Algoritmo87.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo87
+    {
+        /*
+            Crie um programa que melhore o procedimento Gerador() da questão anterior
+            para que mostre uma mensagem personalizada, passada como parâmetro.
+
+            Ex: Ao chamar Gerador("Aprendendo Portugol") aparece:
+            +-------=======------+
+             Aprendendo C#
+            +-------=======------+
+        */
+
+        public static void Start()
+        {
+            Gerador("Aprendendo C#");
+        }
+
+        private static void Gerador(string mensagem)
+        {
+            Console.WriteLine("+-------=======------+");
+            Console.WriteLine(mensagem);
+            Console.WriteLine("+-------=======------+");
+        }
+    }
+}
