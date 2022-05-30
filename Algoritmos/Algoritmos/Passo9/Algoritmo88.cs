f381bccc4aeb38e422e6562a00602c4e978c0f18
Daniel Cruz <d.afc98@gmail.com>
2022-05-29 23:21:08 -0300

    algoritmo 88

---
+++ Algoritmos/Algoritmos/Passo9/Algoritmo88.cs
@@ -0,0 +1,32 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo88
+    {
+        /*
+            Crie um programa que melhore o procedimento Gerador() da questão anterior
+            para que mostre uma mensagem vário
+            Ex: Ao chamar Gerador("Aprendendo C#", 4) aparece:
+            +-------=======------+
+             Aprendendo C#
+             Aprendendo C#
+             Aprendendo C#
+             Aprendendo C#
+            +-------=======------+
+        */
+
+        public static void Start()
+        {
+            Gerador("Aprendendo C#", 4);
+        }
+
+        private static void Gerador(string mensagem, int qtd)
+        {
+            Console.WriteLine("+-------=======------+");
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+            Console.WriteLine("+-------=======------+");
+        }
+    }
+}
