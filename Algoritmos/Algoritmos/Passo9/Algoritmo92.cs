03b5ff49c9b05e70802c271dff462bc6ba279870
Daniel Cruz <d.afc98@gmail.com>
2022-05-30 23:25:15 -0300

    algoritmo 92

---
+++ Algoritmos/Algoritmos/Passo9/Algoritmo92.cs
@@ -0,0 +1,27 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo92
+    {
+        /*
+            Crie uma lógica que leia um número inteiro e passe para um procedimento
+            ParOuImpar() que vai verificar e mostrar na tela se o valor passado como
+            parâmetro é PAR ou ÍMPAR.
+        */
+
+        public static void Start()
+        {
+            Console.Write("Digite um número inteiro: ");
+            var numero = int.Parse(Console.ReadLine() ?? "0");
+
+            ParOuImpar(numero);
+        }
+
+        private static void ParOuImpar(double numero) 
+        {
+            if (numero % 2 == 0)
+                Console.WriteLine($"O número {numero} é PAR");
+            else
+                Console.WriteLine($"O número {numero} é IMPAR");
+        }
+    }
+}
