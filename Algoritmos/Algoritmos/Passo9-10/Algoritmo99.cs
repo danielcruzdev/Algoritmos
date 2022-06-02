3906d3931ddc2f07432ca3c1af047153a8c886b7
Daniel Cruz <d.afc98@gmail.com>
2022-06-01 23:25:20 -0300

    algoritmo 99

---
+++ Algoritmos/Algoritmos/Passo9-10/Algoritmo99.cs
@@ -0,0 +1,25 @@
+﻿namespace Algoritmos.Algoritmos
+{
+    public static class Algoritmo99
+    {
+        /*
+             Faça um programa que possua uma função chamada Potencia(), que vai receber
+            dois parâmetros numéricos (base e expoente) e vai calcular o resultado da
+            exponenciação.
+            Ex: Potencia(5,2) vai calcular 52 = 25 
+        */
+
+
+        public static void Start()
+        {
+            var resultado = Potencia(12, 2);
+
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+
+        private static double Potencia(int numero, int expoente)
+        {
+            return Math.Pow(numero, expoente);
+        }
+    }
+}
